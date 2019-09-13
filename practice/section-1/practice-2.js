76e11a84eb9a50949f988304d235b9bb2ca3f299
NICOLCH <christoper.nicolas@oocl.com>
2019-09-13 18:08:41 +0800

    Section-1 completed

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var lists = [];
+  for(var x = 0; x < collectionA.length; x++){
+	for(var y = 0; y < collectionB.length; y++){
+		if(collectionB[y].indexOf(collectionA[x]) > -1){
+			lists.push(collectionA[x]);
+		}
+	}
+  }
+  return lists;
 }
--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var lists = [];
+  for(var x = 0; x < collectionA.length; x++){
+	for(var y = 0; y < objectB.value.length; y++){
+		if(objectB.value[y].indexOf(collectionA[x]) > -1){
+			lists.push(collectionA[x]);
+		}
+	}
+  }
+  return lists;
 }
