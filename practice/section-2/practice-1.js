9e6778a94c4b9e4c55c2de78e81d601cd4754ab0
abc <abc@example.com>
2019-09-15 14:52:21 +0800

    section-2 completed

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,26 +1,28 @@
 'use strict';
 
 function countSameElements(collection) {
-    var lists = [];
+   var lists = [];
     var key = '';
-    var count = 0;
+    var count;
     for(var x = 0; x < collection.length; x++){
-        if(key == ''){
-            key = collection[x];
+        count = 0;
+        key = collection[x];
+        var y = x;
+        while(y < collection.length){
+            var key2 = collection[y];
+            if(key.indexOf(key2) > -1) {
+                count += 1;   
+            } else{
+                lists.push({key, count});
+                break;  
+            }
+            y++;
         }
-        if(collection[x].indexOf(key) > -1){
-            count += 1;
-            if(x + 1 == collection.length){
-            	lists.push({key, count});
-            }
-        } else{
+        x = y - 1;
+        if(x + 1 == collection.length){
             lists.push({key, count});
-            key = collection[x];
-            count = 1;
-            if(x + 1 == collection.length){
-            	lists.push({key, count});
-            }
         }
     }
-return lists;
+
+    return lists;
 }
